20785b65db0635d7141623439763b020b3d4206e
路人貓 <mingshin123456789@gmail.com>
2022-04-06 11:25:32 +0800

    更新註解

--- Big_Data_Analytics/3. R programming/p122.r
+++ Big_Data_Analytics/3. R programming/p122.r
@@ -3,4 +3,4 @@ freqsum = titanic %>% select(Freq) %>% sum()#sum(titanic$Freq)
 titanic %>% 
 select(Sex,Age,Freq) %>% 
 mutate(portion= Freq/freqsum)
-new_titanic = titanic %>% mutate(portion= Freq/freqsum)
+new_titanic = titanic %>% mutate(portion= Freq/freqsum)#新增一列portion並且其數值為Frep/freqsum
