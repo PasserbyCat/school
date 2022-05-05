03f5bbafeb1c07b71f5f0b8be13d9db7b0632f23
路人貓 <mingshin123456789@gmail.com>
2022-05-05 20:19:03 +0800

    Revert "week8完成Rplot"
    This reverts commit fed86c6cbae23e97fe99b8a504c37495855f6110.

--- Big_Data_Analytics/4. Rplot/p71.r
+++
@@ -1,5 +0,0 @@
-library(leaflet)
-m = leaflet()
-m = setView(m,lng = 121.51, lat = 25.05, zoom = 12)
-m = addTiles(m) # Add default OpenStreetMap map tiles
-m
--- Big_Data_Analytics/4. Rplot/p72.r
+++
@@ -1,5 +0,0 @@
-library(leaflet)
-m = leaflet()
-m = addTiles(m) # Add default OpenStreetMap map tiles
-m = addMarkers(m, lng=121.51, lat=25.05, popup="Taipei Station")
-m#新增座標點
